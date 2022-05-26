6d906a584e84bf2bb73018e109968d8904538b8f
Anderson Tabaldi <anderson.tabaldi@cwi.com.br>
2022-05-26 14:23:47 -0300

    test: Adiciona testes unitários

---
+++ Soft.Taxa.Juros/Soft.Taxa.Juros.TestesUnitários/Api/Controllers/TaxaJurosControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Soft.Taxa.Juros.Api.Controllers;
+using Soft.Taxa.Juros.Domain.TaxaJuro;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Soft.Taxa.Juros.TestesUnitários.Api.Controllers
+{
+    [TestFixture]
+    public class TaxaJurosControllerTests
+    {
+        private Mock<IMediator> _mockMediator;
+        private TaxaJurosController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockMediator = new Mock<IMediator>(MockBehavior.Strict);
+            _controller = new TaxaJurosController(_mockMediator.Object);
+        }
+
+        [Test]
+        public void Deve_VerificarAtributos_Quando_BuscarTaxaDeJuros()
+        {
+            //Assert
+            typeof(TaxaJurosController)
+               .GetMethod(nameof(TaxaJurosController.ObterTaxaDeJuros))
+               .Should()
+               .BeDecoratedWith<HttpGetAttribute>();
+        }
+
+        [Test]
+        public async Task Deve_RetornarOk_Quando_BuscarTaxaDeJuros()
+        {
+            //Arrange
+            var query = new TaxaJuroQuery();
+
+            _mockMediator
+                .Setup(p => p.Send(It.IsAny<TaxaJuroQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(default(decimal));
+
+            // Act
+            var result = await _controller.ObterTaxaDeJuros();
+
+            // Assert
+            var response = result.Should().BeOfType<OkObjectResult>().Subject.Value;
+            response.Should().BeOfType<decimal>();
+
+            _mockMediator.Verify(p => p.Send(It.IsAny<TaxaJuroQuery>(), It.IsAny<CancellationToken>()));
+        }
+    }
+}
---
+++ Soft.Taxa.Juros/Soft.Taxa.Juros.TestesUnitários/Domain/TaxaJuro/TaxaJuroQueryHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using Soft.Taxa.Juros.Domain.TaxaJuro;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Soft.Taxa.Juros.TestesUnitários.Domain.TaxaJuro
+{
+    [TestFixture]
+    public class TaxaJuroQueryHandlerTests
+    {
+        public Mock<IConfiguration> _mockConfiguration;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockConfiguration = new Mock<IConfiguration>(MockBehavior.Strict);
+        }
+
+        [Test]
+        public async Task Deve_RetornarTaxaDeJuro_Quando_BuscarTaxaDeJuros()
+        {
+            // Arrange
+            decimal taxaJuros = 0.01M;
+
+            _mockConfiguration
+                .Setup(p => p["TaxaDeJuros"])
+                .Returns(taxaJuros.ToString());
+
+            // Act
+            var result = await BuscarHandler().Handle(new TaxaJuroQuery(), It.IsAny<CancellationToken>());
+
+            // Assert
+            result.Should().Be(taxaJuros);
+        }
+
+        [Test]
+        public void Deve_RetornarArgumentOutOfRangeException_Quando_BuscarTaxaDeJuros_Com_ValorConfiguradoInvalido()
+        {
+            // Arrange
+            _mockConfiguration
+                .Setup(p => p["TaxaDeJuros"])
+                .Returns("mock-invalid-value");
+
+            // Act
+            var result = Assert.Throws<ArgumentOutOfRangeException>(() => BuscarHandler().Handle(new TaxaJuroQuery(), It.IsAny<CancellationToken>()));
+
+            // Assert
+            result.Message.Should().Contain("A taxa de juros configurada está com um valor incorreto!");
+        }
+
+        private TaxaJuroQueryHandler BuscarHandler()
+        {
+            return new TaxaJuroQueryHandler(_mockConfiguration.Object);
+        }
+    }
+}
